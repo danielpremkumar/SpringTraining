a8b2a79ac9a8be3e5077710aab3c60e03b1551d8
nerdylegend <31123377+nerdylegend@users.noreply.github.com>
2019-08-31 02:34:15 +0530

    Updates

--- src/main/java/com/nerdylegend/rest/webservices/restfulwebservices/swagger/config/SwaggerConfig.java
+++ src/main/java/com/nerdylegend/rest/webservices/restfulwebservices/swagger/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.nerdylegend.rest.webservices.restfulwebservices.swagger.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -10,8 +11,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo();
+
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO);
+
     }
 }
